7dcaa1759170e88e2998168d9567f989720567eb
Lou <l_saint_andre@hotmail.com>
2021-10-26 15:37:27 -0400

    moreChanges

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ app.getSomething = function(){
 
     $('.playHand').on('click', function(){
         const html1 = `
-            < class="">You Lose!
-            HAHAHAHAHAHAHA</p>
+            <h1>You Lose!
+            HAHAHAHAHAHAHA</h1>
         `
         $('.card').empty();
         $('h1').replaceWith(html1);
